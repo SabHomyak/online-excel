50512483873e9119371cbe52a4ab8f905b2d1c31
Vadim Bobylov <davyjonessss@ukr.net>
2020-12-16 22:41:01 +0200

    convert html to react component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './dashboard.module.scss'
 import HeaderContainer from "./Header/HeaderContainer";
+import TableContainer from "./Table/TableContainer";
 
 const Dashboard = (props) => {
     return (
@@ -13,26 +14,7 @@ const Dashboard = (props) => {
                    </a>
                </div>
             </div>
-            <div className={classes.table + ' ' + classes.view}>
-                <div className={classes.listHeader}>
-                    <span>Название</span>
-                    <span>Дата открытия</span>
-                </div>
-                <ul className={classes.list}>
-                    <li className={classes.record}>
-                        <a href="#">Таблица номер 1</a>
-                        <strong>12.06.2020</strong>
-                    </li>
-                    <li className={classes.record}>
-                        <a href="#">Таблица номер 1</a>
-                        <strong>12.06.2022</strong>
-                    </li>
-                    <li className={classes.record}>
-                        <a href="#">Таблица номер 1</a>
-                        <strong>12.06.2021</strong>
-                    </li>
-                </ul>
-            </div>
+            <TableContainer/>
         </div>
     )
 }
